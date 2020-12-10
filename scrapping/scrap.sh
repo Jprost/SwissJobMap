c634c6322cf0259ad43f183b893e05d817ccbfb7
Jprost <prost.jb@gmail.com>
2020-12-10 12:07:51 +0100

    $currentDate

---
+++ scrapping/scrap.sh
@@ -0,0 +1,17 @@
+while true; 
+do
+	HOUR="$(($(date +'%-H') % 3))"
+	if [ $HOUR -eq 0 ]; then
+		echo Scrapping ...		
+		python3 scrap.py
+		git pull
+		git add ../Data/df_jobs_content.p ../Data/df_jobs.p
+		git commit -m `date +"%a-%d-%b %H"`
+		git push
+
+		sleep 1h
+	else
+		echo Sleeping ...
+		sleep 30m
+	fi
+done
