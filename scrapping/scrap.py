d6a0216e02ba0a14bc12d4f19b1ab0053cd66427
jprost <jean-baptiste.prost@epfl.ch>
2020-12-10 15:42:44 +0100

    New logging

--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -1,18 +1,17 @@
-import logging
+import logging.config
+import yaml
 import pandas as pd
 from scrapper import Scrapper
-from data_formatting import get_job_functions, canton_cleaning, split_data_frame, save_dataframes
-
+from data_formatting import get_job_functions, canton_cleaning, split_data_frame
 """
 Get the publicly available data from LinkedIn and formats it in a DataFrame fro the interactive web app. 
 
 The whole program process is written int eh scrapper_process.log log file.
 """
+with open('logging_config.yml', 'r') as config:
+    logging.config.dictConfig(yaml.safe_load(config))
 
-logging.basicConfig(filename='scapper_process.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    datefmt='%d-%b-%y %H:%M:%S')
-
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     linkedin_link = 'https://www.linkedin.com/jobs/search/?location=Switzerland&sortBy=DD'
@@ -24,6 +23,4 @@ if __name__ == '__main__':
     df_raw = get_job_functions(df_raw)
     df_raw = canton_cleaning(df_raw, '../Data/')
     df_jobs, df_content = split_data_frame(df_raw)
-    
-    #saving
-    save_dataframes(df_jobs, df_content)
+
